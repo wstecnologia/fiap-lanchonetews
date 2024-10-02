f21479720d7f3598a44d7cc1484f0b9d99c2f0cc
saviodba <saviodba@gmail.com>
2024-10-02 15:32:59 -0300

    first commit

---
+++ src/core/adapters/dtos/PaymentDto.ts
@@ -0,0 +1,15 @@
+
+import { PaymentStatus } from "@/core/shared/constants/PaymentStatus"
+
+export interface inputPaymentDto {
+  orderid: string
+  amount: number
+  status: PaymentStatus
+}
+
+export interface outputPaymentDto {
+  id: string
+  orderid: string
+  amount: number
+  status: PaymentStatus
+}
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import app from "./config/Server";
+
+const PORT = process.env.PORT || '3000';
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
